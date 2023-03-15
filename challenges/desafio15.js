c6b39525dd7293c7dbb1a058af9bf695c1ed62c6
Thiago Souza <thiago29.06@hotmail.com>
2023-03-15 18:22:57 -0300

    feat: query do requisito15

---
+++ challenges/desafio15.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt(0) } },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  { $set: { avaliacao: NumberInt(5) } },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["ave"] } },
+  { $set: { avaliacao: NumberInt(3) } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, avaliacao: 1 },
+);
